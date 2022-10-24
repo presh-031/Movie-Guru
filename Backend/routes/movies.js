ee11abf1b54078198ec1a3ba5121031459cb2ede
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-24 20:23:59 +0100

    tested server routes

---
+++ Backend/routes/movies.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+// GET a movie
+
+// GET Top rated movies
+
+// GET trending movies
+
+// POST a new movie
+
+// UPDATE a movie
+
+//  DELETE a movie
+
+//  GET movie categories
+
+//  GET all movies in a category
