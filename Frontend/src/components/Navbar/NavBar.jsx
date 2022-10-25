7c144bad4a2da7712c5f45c2bdd073536c93a5b6
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-25 10:52:41 +0100

    now styling navbar

--- Frontend/src/components/Navbar/NavBar.jsx
+++ Frontend/src/components/Navbar/NavBar.jsx
@@ -1,6 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 import css from "./NavBar.module.css";
 const NavBar = () => {
-  return <div className={css.container}>NavBar</div>;
+  return (
+    <header className={css.header}>
+      <p>MOVIE GURU</p>
+      <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/categories">Categories</NavLink>
+        <NavLink to="/add-movie">Add Movies</NavLink>
+      </div>
+    </header>
+  );
 };
 
 export default NavBar;
