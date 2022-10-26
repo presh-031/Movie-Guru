58f9fa0006d82ed772c673fd475d8fa26995a982
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-26 06:26:29 +0100

    now styling movies showing on categories page

---
+++ Frontend/src/components/Category/Category.jsx
@@ -0,0 +1,11 @@
+import css from "./Category.module.css";
+
+const Category = () => {
+  return (
+    // <div className={css.category_container}>
+    <p className={css.category}>Action</p>
+    // </div>
+  );
+};
+
+export default Category;
--- Frontend/src/pages/Categories.jsx
+++ Frontend/src/pages/Categories.jsx
@@ -1,5 +1,42 @@
+import Category from "../components/Category/Category";
+import MovieCard from "../components/MovieCard/MovieCard";
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const categories = {
+    width: "90%",
+    margin: "0 auto",
+    // border: "1px solid red",
+  };
+
+  const heading = {
+    width: "90%",
+    margin: "4rem auto",
+    color: "var(--whitish)",
+    fontSize: "2rem",
+    fontWeight: "600",
+  };
+
+  return (
+    <>
+      <p style={heading}>Categories</p>
+      <div style={categories}>
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+        <Category />
+      </div>
+      {/* The category selected with its movies show below. */}
+      <p>Top Rated</p>
+      <div>
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    </>
+  );
 };
 
 export default Categories;
