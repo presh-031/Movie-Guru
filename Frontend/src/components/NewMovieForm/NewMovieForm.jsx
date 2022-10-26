0f01f93ae58e8f64d7c23cbe3682ffb13a4f7980
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-26 18:47:18 +0100

    form components now work

--- Frontend/src/components/NewMovieForm/NewMovieForm.jsx
+++ Frontend/src/components/NewMovieForm/NewMovieForm.jsx
@@ -1,61 +1,142 @@
+import { useState } from "react";
+import { act } from "react-dom/test-utils";
 import css from "./NewMovieForm.module.css";
 
 const NewMovieForm = () => {
+  const [title, setTitle] = useState("");
+  const [director, setDirector] = useState("");
+  const [year, setYear] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [synopsis, setSynopsis] = useState("");
+
+  const [action, setAction] = useState(false);
+  const [comedy, setComedy] = useState(false);
+  const [drama, setDrama] = useState(false);
+  const [fantasy, setFantasy] = useState(false);
+  const [horror, setHorror] = useState(false);
+  const [romance, setRomance] = useState(false);
+  const [thriller, setThriller] = useState(false);
+
+  console.count("times : ");
+
+  // Handlers for text inputs and textareas.
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
+  }
+  function handleDirectorChange(e) {
+    setDirector(e.target.value);
+  }
+  function handleYearChange(e) {
+    setYear(e.target.value);
+  }
+  function handleImageChange(e) {
+    setImageUrl(e.target.value);
+  }
+  function handleSynopsisChange(e) {
+    setSynopsis(e.target.value);
+  }
+
+  // Handlers for checkboxes
+  function handleActionChange() {
+    console.log(!action);
+    setAction((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+
+  function handleComedyChange() {
+    console.log(!comedy);
+    setComedy((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+  function handleDramaChange() {
+    console.log(!comedy);
+    setDrama((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+  function handleFantasyChange() {
+    console.log(!fantasy);
+    setFantasy((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+  function handleHorrorChange() {
+    console.log(!horror);
+    setHorror((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+  function handleRomanceChange() {
+    console.log(!romance);
+    setRomance((prevChecked) => {
+      return !prevChecked;
+    });
+  }
+
+  function handleThrillerChange() {
+    console.log(!thriller);
+    setThriller((prevChecked) => {
+      return !prevChecked;
+    });
+  }
   return (
     <form>
       <div className={css.first_section}>
         <label htmlFor="title">Title</label>
-        <input type="text" name="title" id="title" />
+        <input value={title} onChange={handleTitleChange} type="text" name="title" id="title" />
 
         <label htmlFor="director">Director</label>
-        <input type="text" name="director" id="director" />
+        <input value={director} onChange={handleDirectorChange} type="text" name="director" id="director" />
 
         <label htmlFor="year">Year</label>
-        <input type="number" name="year" id="year" />
+        <input value={year} onChange={handleYearChange} type="number" name="year" id="year" />
 
         <label htmlFor="image">Image URL</label>
-        <input type="text" name="image" id="image" />
+        <input value={imageUrl} onChange={handleImageChange} type="text" name="image" id="image" />
 
         <label htmlFor="synopsis">Synopsis</label>
-        <textarea name="synopsis" id="synopsis" rows="10"></textarea>
+        <textarea value={synopsis} onChange={handleSynopsisChange} name="synopsis" id="synopsis" rows="10"></textarea>
       </div>
+      {/* Checkboxes section */}
       <p>Categories</p>
       <div className={css.second_section}>
         <div>
-          <input type="checkbox" name="action" id="action" />
+          <input checked={action} onChange={handleActionChange} type="checkbox" name="action" id="action" />
           <label htmlFor="action">Action</label>
         </div>
 
         <div>
-          <input type="checkbox" name="comedy" id="comedy" />
+          <input checked={comedy} onChange={handleComedyChange} type="checkbox" name="comedy" id="comedy" />
           <label htmlFor="comedy">Comedy</label>
         </div>
 
         <div>
-          <input type="checkbox" name="drama" id="drama" />
+          <input checked={drama} onChange={handleDramaChange} type="checkbox" name="drama" id="drama" />
           <label htmlFor="drama">Drama</label>
         </div>
 
         <div>
-          <input type="checkbox" name="fantasy" id="fantasy" />
+          <input checked={fantasy} onChange={handleFantasyChange} type="checkbox" name="fantasy" id="fantasy" />
           <label htmlFor="fantasy">Fantasy</label>
         </div>
 
         <div>
-          <input type="checkbox" name="horror" id="horror" />
+          <input checked={horror} onChange={handleHorrorChange} type="checkbox" name="horror" id="horror" />
           <label htmlFor="horror">Horror</label>
         </div>
 
         <div>
-          <input type="checkbox" name="romance" id="romance" />
+          <input checked={romance} onChange={handleRomanceChange} type="checkbox" name="romance" id="romance" />
           <label htmlFor="romance">Romance</label>
         </div>
 
         <div>
-          <input type="checkbox" name="thriller" id="thriller" />
+          <input checked={thriller} onChange={handleThrillerChange} type="checkbox" name="thriller" id="thriller" />
           <label htmlFor="thriller">Thriller</label>
         </div>
-      </div>{" "}
+      </div>
       <button>Save movie</button>
     </form>
   );
