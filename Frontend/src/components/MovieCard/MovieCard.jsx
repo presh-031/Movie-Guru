a1bfc7338f700b574dd8b618d6dc7fdc9c27ff94
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-26 12:56:39 +0100

    trending movies and movie card now dynamic

--- Frontend/src/components/MovieCard/MovieCard.jsx
+++ Frontend/src/components/MovieCard/MovieCard.jsx
@@ -2,19 +2,19 @@ import { BsFillPlayFill } from "react-icons/bs";
 
 import css from "./MovieCard.module.css";
 
-const MovieCard = () => {
+const MovieCard = ({ image, title, year }) => {
   return (
     <div className={css.movie_card}>
       <div className={css.poster_container}>
-        <img src="https://image.tmdb.org/t/p/w500//aTovumsNlDjof7YVoU5nW2RHaYn.jpg" alt="" />
+        <img src={image} alt="movie-poster" />
       </div>
       <div className={css.card_info}>
         <div className={css.play_btn}>
           <BsFillPlayFill size={35} style={{ color: "black", opacity: 0.8 }} />
         </div>
         <div>
-          <p className={css.movie_name}>Pulp Fiction</p>
-          <p className={css.movie_year}>1994</p>
+          <p className={css.movie_name}>{title}</p>
+          <p className={css.movie_year}>{year}</p>
         </div>
       </div>
     </div>
--- Frontend/src/components/TrendingMovies/TrendingMovies.jsx
+++ Frontend/src/components/TrendingMovies/TrendingMovies.jsx
@@ -1,29 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import MovieCard from "../MovieCard/MovieCard";
 
 import css from "./TrendingMovies.module.css";
 const TrendingMovies = () => {
+  const [trendingMovies, setTrendingMovies] = useState(null);
   useEffect(() => {
     const getTrendingMovies = async () => {
-      console.log("getting the movies");
+      // console.log("getting the movies");
 
-      const res = await fetch("/api/movies/top-rated");
+      const res = await fetch("/api/movies/trending");
       const data = await res.json();
 
-      console.log(data);
+      if (res.ok) {
+        setTrendingMovies(data);
+      }
     };
     getTrendingMovies();
   }, []);
   return (
     <section className={css.container}>
       <p>Trending Movies</p>
-      <div className={css.cards_container}>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </div>
+      {trendingMovies && (
+        <div className={css.cards_container}>
+          {trendingMovies.map((trendingMovie) => {
+            return (
+              <MovieCard
+                key={trendingMovie._id}
+                image={trendingMovie.image}
+                title={trendingMovie.title}
+                year={trendingMovie.year}
+              />
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 };
