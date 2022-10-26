8d770e0b4168eea40bc8ee9673d8bb5ac98d8786
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-26 13:07:56 +0100

    homepage now displays data from server

--- Frontend/src/components/TopRatedMovies/TopRatedMovies.jsx
+++ Frontend/src/components/TopRatedMovies/TopRatedMovies.jsx
@@ -1,17 +1,40 @@
 import MovieCard from "../MovieCard/MovieCard";
 
+import { useState, useEffect } from "react";
 import css from "./TopRatedMovies.module.css";
 
 const TopRatedMovies = () => {
+  const [topRatedMovies, setTopRatedMovies] = useState(null);
+  useEffect(() => {
+    const getTopRatedMovies = async () => {
+      // console.log("getting the movies");
+
+      const res = await fetch("/api/movies/top-rated");
+      const data = await res.json();
+
+      if (res.ok) {
+        setTopRatedMovies(data);
+      }
+    };
+    getTopRatedMovies();
+  }, []);
   return (
     <section className={css.container}>
       <p>Top Rated</p>
-      <div className={css.cards_container}>
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-      </div>
+      {topRatedMovies && (
+        <div className={css.cards_container}>
+          {topRatedMovies.map((topRatedMovie) => {
+            return (
+              <MovieCard
+                key={topRatedMovie._id}
+                image={topRatedMovie.image}
+                title={topRatedMovie.title}
+                year={topRatedMovie.year}
+              />
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 };
